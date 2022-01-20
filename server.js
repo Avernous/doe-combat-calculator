50e833a12ba2c869a3dcb9384a656bea7bbdd212
Avernous <fouksconnor@gmail.com>
2022-01-20 11:27:11 -0600

    set bufferedcommands to false to help diagnose database connection problems. add db.on disconnect and connect methods

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
-const Faction = require('./models/faction.model.js');
 const Seeder = require('./seeders/seeder');
 const router = require("./routes/routes.js");
 const bodyParser = require('body-parser');
@@ -12,17 +11,25 @@ const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
 var mongoDB = 'mongodb://doe_backend:AldrichHouse80@10.0.1.42/dusk_of_empires';
 
+
+//if buffer commands is set to true and the database is not connected. no errors will be thrown for using model functions
+mongoose.set('bufferCommands', false);
 //Connect to database
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
 .then((result) => { 
-  console.log('connected to DB');
   //Seed the Database
   Seeder();
 }).catch((err) => {
   console.log(err);
 } );
+//mongoose database connection error handling
 var db = mongoose.connection;
+db.on('connected', () => {
+  console.log('Connected to MongoDB')
+
+});
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('disconnected', () => {console.log('MongoDB disconnected!')});
 
 // App
 const app = express();
