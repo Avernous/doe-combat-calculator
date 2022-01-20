f65987ceccb56d477adffe90fa77c0759a48ff53
Avernous <fouksconnor@gmail.com>
2022-01-19 20:49:44 -0600

    change server file to use seperate route files

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Faction = require('./models/faction.model.js');
 const Seeder = require('./seeders/seeder');
+const router = require("./routes/routes.js");
+const bodyParser = require('body-parser');
 
 //Constants
 const PORT = process.env.PORT || 8080;
@@ -25,25 +27,8 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // App
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("<html><h1>Hello World</h1></html>");
-});
-
-app.get('/faction', (req, res) => {
-  let faction = new Faction({
-    faction_name:"The Imperial College",
-    player_name:"Connor F"
-  });
-  faction.save(function (err, results) {
-    console.error(err);
-    if(results._id){
-      res.send(`<html><h1>Faction ID: ${results._id}</h1></html>`);
-    }else{
-      res.send(`<html><h1>Failed</h1></html>`);
-    }
-  });
-  
-});
+app.use(bodyParser.json())
+app.use('/api/v1', router)
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
