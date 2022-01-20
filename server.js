cec1e0ab70ce0f5cd8abf56872b2d8b831437cf2
Avernous <fouksconnor@gmail.com>
2022-01-20 13:13:20 -0600

    move app secrets to .env file and fix seeding to be depedant on the ship collection existing

--- server.js
+++ server.js
@@ -5,28 +5,54 @@ const mongoose = require('mongoose');
 const Seeder = require('./seeders/seeder');
 const router = require("./routes/routes.js");
 const bodyParser = require('body-parser');
+require('dotenv').config({ path: './env/.env' });
 
 //Constants
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 const HOST = '0.0.0.0';
-var mongoDB = 'mongodb://doe_backend:AldrichHouse80@10.0.1.42/dusk_of_empires';
-
+const mongoDBUsername = process.env.MONGO_KEY
+const mongoDBPassword = process.env.MONGO_SECRET
+const mongoDBUrl = process.env.MONGO_DB_URL
+const mongoDBPort = process.env.MONGO_DB_PORT
+const mongoDBName = process.env.MONGO_DB_NAME
+var mongoDB = `mongodb://${mongoDBUsername}:${mongoDBPassword}@${mongoDBUrl}:${mongoDBPort}/${mongoDBName}`;
 
 //if buffer commands is set to true and the database is not connected. no errors will be thrown for using model functions
 mongoose.set('bufferCommands', false);
 //Connect to database
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
 .then((result) => { 
-  //Seed the Database
-  Seeder();
+  
+  
 }).catch((err) => {
   console.log(err);
 } );
-//mongoose database connection error handling
+
+//mongoose database event handling
 var db = mongoose.connection;
 db.on('connected', () => {
-  console.log('Connected to MongoDB')
+  console.log('Connected to MongoDB');
+  console.log('checking if seeding is needed:');
+  mongoose.connection.db.listCollections().toArray(function(err, names) {
+    if (err) {
+        console.log(err);
+    } else {
+      let isSeeded=false;
+      names.forEach((collection, index) =>{
+        if(collection.name=="ships"){
+          isSeeded = true;
+        }
+      });
 
+      if(isSeeded == false){
+        //Seed the Database
+        Seeder();
+      }else{
+        console.log("Seeding Not Required");
+      }
+
+    }
+  });
 });
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.on('disconnected', () => {console.log('MongoDB disconnected!')});
