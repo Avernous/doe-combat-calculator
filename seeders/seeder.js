a02518f4c33e7de1977af5214f7db1750715582a
Avernous <fouksconnor@gmail.com>
2022-01-20 11:22:56 -0600

    add ship seeder to main seeder file

--- seeders/seeder.js
+++ seeders/seeder.js
@@ -1,6 +1,8 @@
 const aptitudeSeeder = require('./combat_modifier.seeder');
+const shipSeeder = require('./ship.seeder');
 
 module.exports = function (){
     console.log("Now Seeding:")
     aptitudeSeeder();
+    shipSeeder();
 }
